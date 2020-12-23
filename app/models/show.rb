33c033fe9c55aa6f8e82fc07a362a7b05278f7db
Arif Cagirici <acagirici23@gmail.com>
2020-12-23 05:04:29 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
   
   def Show::highest_rating
-    
+    Show.maximum(:rating)
   end
   
   def Show::most_popular_show
@@ -9,7 +9,7 @@ class Show < ActiveRecord::Base
   end
   
   def Show::lowest_rating
-    
+    Show.minimum(:rating)
   end
   
   def Show::least_popular_show
@@ -24,9 +24,8 @@ class Show < ActiveRecord::Base
     
   end
   
-  def Show::highest_rating
+  def Show::shows_by_alphabetical_order
     
   end
-  
   
 end
